05dbf22b8551c6bbde3094d2a445b08ef385c214
savindu29 <1savindupanagoda@gmail.com>
2023-10-12 11:49:23 +0530

    fist commit

---
+++ src/main/java/com/ecom/sketch/entity/Product.java
@@ -0,0 +1,12 @@
+package com.ecom.sketch.entity;
+
+public class Product {
+    String id;
+    String name;
+    String sex;
+    String size;
+    String description;
+    int qty;
+    double price;
+
+}
