6924f46a37cc7c2e40e6b09b7f2a85459138f32d
savindu29 <1savindupanagoda@gmail.com>
2023-10-12 11:42:20 +0530

    fist commit

---
+++ src/main/java/com/ecom/sketch/SketchApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.sketch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SketchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SketchApplication.class, args);
+	}
+
+}
